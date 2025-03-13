53d3c968e41a6eb86e047dbf5b944a73c6c369b2
queensley200 <105300323+queensley200@users.noreply.github.com>
2025-03-13 15:52:59 +0100

    Updateec2 public in main outputs.tf

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -3,7 +3,7 @@ output "vpc_id" {
 }
 
 output "ec2_public_ips" {
-  description = "Public IPs of the created EC2 instances"
-  value       = { for key, instance in module.ec2 : key => instance.public_ips }
+  value = module.ec2.public_ips
 }
+
 
